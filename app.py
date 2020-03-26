25014c36ccfe97602798fd7a502b519bc1cff641
Akshay199456 <akshay.kum94@gmail.com>
2020-03-25 22:31:15 -0500

    Return pretty json

--- app.py
+++ app.py
@@ -12,17 +12,25 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 print(os.environ['APP_SETTINGS'])
 
-
 @app.route('/')
+def home():
+	return 'Hello World!'
+
+@app.route('/states')
 def hello():
 	response = requests.get('https://covidtracking.com/api/states')
 	if response:
 		print('Response: ', response)
+		# json.loads takes in a string and returns a json object
 		json_object = json.loads(response.text)
-		return json.dumps(json_object, indent=2)
+		# The jsonify() function in flask returns a flask.Response() object 
+		# that already has the appropriate content-type header 'application/json' 
+		# for use with json responses. Whereas, the json.dumps() method will just return 
+		# an encoded string, which would require manually adding the MIME type header.
+		return jsonify(json_object)
 	else:
 		print('Error occurred!')
-		return "Hello World!"
+		return "Error"
 
 
 @app.route('/<name>')
