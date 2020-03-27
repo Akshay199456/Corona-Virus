d3606db7009c7a7f278a4cf91f24bbb702e9b5bf
Akshay199456 <akshay.kum94@gmail.com>
2020-03-27 15:16:58 -0500

    Get data for states

--- app.py
+++ app.py
@@ -12,17 +12,49 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 print(os.environ['APP_SETTINGS'])
 
-
 @app.route('/')
+def home():
+	return 'Hello World!'
+
+@app.route('/states')
 def hello():
 	response = requests.get('https://covidtracking.com/api/states')
 	if response:
 		print('Response: ', response)
+		# json.loads takes in a string and returns a json object
 		json_object = json.loads(response.text)
-		return json.dumps(json_object, indent=2)
+		# The jsonify() function in flask returns a flask.Response() object 
+		# that already has the appropriate content-type header 'application/json' 
+		# for use with json responses. Whereas, the json.dumps() method will just return 
+		# an encoded string, which would require manually adding the MIME type header.
+		return jsonify(json_object)
 	else:
 		print('Error occurred!')
-		return "Hello World!"
+		return "Error"
+
+
+@app.route('/api/<state>')
+def api(state):
+	'''
+	Getting data from http://coronavirusapi.com/
+	'''
+	response = requests.get('http://coronavirusapi.com/getTimeSeries/' + state)
+	if response:
+		print('Response from api: ', response)
+		# Data returned from api in 'str' format
+		text_response = json.dumps(response.text)
+		print('Response text from api: ', text_response)
+		if(text_response == '"Please use a 2 letter state abbreviation"'):
+			return 'Invalid State'
+		else:
+			# Tokenize the data
+			all_data = response.text.split('\n')
+			print('All data: ', all_data)
+			return 'State: ' + state
+	else:
+		print('Error occurred!')
+		return "Error"
+
 
 
 @app.route('/<name>')
